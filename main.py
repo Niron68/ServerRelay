5e669dd3da4bcc23f4b5c4a4918211a5de64776b
benjamin.b <boivinbenjamin2@gmail.com>
2021-01-30 14:31:58 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import requests
+
+PORT = 3001
+HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
+
+app = Flask(__name__)
+
+
+def requestAPI(method, port, subpath, payload):
+    if method in HTTP_METHODS and method != 'GET':
+        r = getattr(requests, method.lower())(
+            f'http://localhost:{port}/{subpath}', data=payload)
+        return r.text
+    elif method == 'GET':
+        r = requests.get(f'http://localhost:{port}/{subpath}', params=payload)
+        return r.text
+    else:
+        return f'Unauthorized methods: {method}'
+
+
+@app.route("/")
+def main():
+    return "Main page"
+
+
+@app.route("/<int:port>/<path:subpath>", methods=HTTP_METHODS)
+def server_route(port, subpath):
+    method = request.method
+    if method == 'GET':
+        return requestAPI(method, port, subpath, request.args)
+    elif method in HTTP_METHODS and method != 'GET':
+        body = request.data
+        if body == b'':
+            body = request.form
+        return requestAPI(method, port, subpath, body)
+    return "Done"
+
+
+app.run(port=PORT)
